cc112b024bf089e8e786ad58dcd2325dac427a29
Tap Payments <o.rabie@tap.company>
2020-05-11 11:14:21 +0200

    1-  Added base files. 2- Updated to latest swift. 3- Updates to latest pods versions and fixes

---
+++ TapNetworkKit-iOS/Pods/Target Support Files/Pods-TapNetworkKit-iOS/Pods-TapNetworkKit-iOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_TapNetworkKit_iOS : NSObject
+@end
+@implementation PodsDummy_Pods_TapNetworkKit_iOS
+@end
---
+++ TapNetworkKit-iOS/TapNetworkKit-iOS/TapNetworkKit_iOS.h
@@ -0,0 +1,19 @@
+//
+//  TapNetworkKit_iOS.h
+//  TapNetworkKit-iOS
+//
+//  Created by Osama Rabie on 11/05/2020.
+//  Copyright © 2020 Tap Payments. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TapNetworkKit_iOS.
+FOUNDATION_EXPORT double TapNetworkKit_iOSVersionNumber;
+
+//! Project version string for TapNetworkKit_iOS.
+FOUNDATION_EXPORT const unsigned char TapNetworkKit_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TapNetworkKit_iOS/PublicHeader.h>
+
+
